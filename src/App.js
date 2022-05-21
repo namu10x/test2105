adec0df88c4daff507d46ea0530353692cb48b50
namu10x <namphamctn1303@gmail.com>
2022-05-21 19:30:37 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nam: true,
+      nam2: false,
+      a: 0,
+    };
+  }
+
+  render() {
+    return <div>hi</div>;
+  }
+}
+
+export default App;
---
+++ src/components/Xinchao.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Xinchao extends Component {
+  render() {
+    return (
+      <div>Xinchao</div>
+    )
+  }
+}
